b4b986f9b183e25fc37f275318511f73e8b6ac22
Isaac Lovern Kamara, Jr <www.isaacjrkamara25@gmail.com>
2023-11-16 15:16:53 +0000

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,62 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { LinearGradient } from "expo-linear-gradient";
+import Animated, { FadeIn, FadeInDown, FadeOut } from "react-native-reanimated";
+
+const Index = () => {
+  return (
+    <View className="flex-1 justify-end">
+      <StatusBar style="light" />
+
+      <Image
+        source={require("../assets/images/welcome.png")}
+        className="h-full w-full absolute"
+      />
+      <LinearGradient
+        colors={["transparent", "#18181b"]}
+        style={{ width: wp(100), height: hp(70) }}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 0.8 }}
+        className="flex justify-end pb-12 space-y-8"
+      >
+        <Animated.View
+          entering={FadeInDown.delay(100).springify()}
+          className="flex items-center"
+        >
+          <Text
+            className="text-white font-bold tracking-widest"
+            style={{ fontSize: hp(5) }}
+          >
+            Best <Text className="text-rose-500">Workout</Text>
+          </Text>
+          <Text
+            className="text-white font-bold tracking-widest"
+            style={{ fontSize: hp(3) }}
+          >
+            For You
+          </Text>
+        </Animated.View>
+        <Animated.View entering={FadeInDown.delay(200).springify()}>
+          <TouchableOpacity
+            style={{ height: hp(7), width: wp(80) }}
+            className="bg-rose-500 flex items-center justify-center mx-auto rounded-full "
+          >
+            <Text
+              className="text-white font-bold tracking-widest"
+              style={{ fontSize: hp(3.4) }}
+            >
+              Get Started
+            </Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </LinearGradient>
+    </View>
+  );
+};
+
+export default Index;
